265fd747377b5cc59154a1a63c5da12c70b64711
raphaeltimbo <raphaelts@gmail.com>
2018-04-17 14:34:24 -0300

    Add test for exception error message.

---
+++ prf2/point.py
@@ -0,0 +1,29 @@
+
+class Point:
+    """Point.
+    A point in the compressor map that can be defined in different ways.
+    Parameters
+    ----------
+    speed : float
+        Speed in 1/s.
+    flow_v or flow_m : float
+        Volumetric or mass flow.
+    suc, disch : prf.State, prf.State
+        Suction and discharge states for the point.
+    suc, head, eff : prf.State, float, float
+        Suction state, polytropic head and polytropic efficiency.
+    suc, head, power : prf.State, float, float
+        Suction state, polytropic head and gas power.
+    suc, eff, vol_ratio : prf.State, float, float
+        Suction state, polytropic efficiecy and volume ratio.
+    Returns
+    -------
+    Point : prf.Point
+        A point in the compressor map.
+    """
+    def __init__(self, *args, speed, **kwargs):
+        flow_v = kwargs.get('flow_v', None)
+        flow_m = kwargs.get('flow_m', None)
+        if flow_v is None and flow_m is None:
+            raise TypeError("__init__() missing 1 required keyword-only"
+                            " argument: 'flow_v' or 'flow_m'.")
--- prf2/tests/test_point.py
+++ prf2/tests/test_point.py
@@ -1,11 +1,39 @@
 import pytest
 from prf2 import Q_
 from prf2.state import State
+from prf2.point import Point
 
 
 @pytest.fixture
-def suc_1():
+def suc_0():
     fluid = dict(CarbonDioxide=0.76064, R134a=0.23581,
                  Nitrogen=0.00284, Oxygen=0.00071)
-    return State.define(p=Q_(1.839, 'bar'), T=Q_(291.5), fluid=fluid)
+    return State.define(p=Q_(1.839, 'bar'), T=291.5, fluid=fluid)
+
+
+@pytest.fixture
+def disch_0():
+    fluid = dict(CarbonDioxide=0.76064, R134a=0.23581,
+                 Nitrogen=0.00284, Oxygen=0.00071)
+    return State.define(p=Q_(5.902, 'bar'), T=380.7, fluid=fluid)
+
+
+def test_speed_not_given(suc_0, disch_0):
+    with pytest.raises(TypeError) as ex:
+        Point(suc=suc_0, disch=disch_0, flow_v=0)
+    assert "missing 1 required keyword-only argument: 'speed'" \
+           in ex.__str__()
+
+
+def test_flow_not_given(suc_0, disch_0):
+    with pytest.raises(TypeError) as ex:
+        Point(suc=suc_0, disch=disch_0, speed=0)
+    assert "missing 1 required keyword-only argument: 'flow_v' or 'flow_m'." \
+           in ex.__str__()
+
+
+@pytest.fixture
+def point_0():
+    assert 0
+
 
