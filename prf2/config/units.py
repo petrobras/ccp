b1eb7c481c188cdac643b241e07a4509ab0d702f
raphaeltimbo <raphaelts@gmail.com>
2018-04-17 11:23:39 -0300

    Fix bug from kwargs that were not properties with units.
    The missing else statement would cause base_unit_kwargs to end up
    without args such as 'fluid'.

--- prf2/config/units.py
+++ prf2/config/units.py
@@ -6,7 +6,6 @@ units = {'p': 'pascal', 'T': 'kelvin'}
 def check_units(func):
     """Wrapper to check and convert units to base_units."""
     def inner(*args, **kwargs):
-
         base_unit_kwargs = {}
         for k, v in kwargs.items():
             if k in units:
@@ -14,6 +13,8 @@ def check_units(func):
                     base_unit_kwargs[k] = v.to(units[k])
                 except AttributeError:
                     base_unit_kwargs[k] = Q_(v, units[k])
+            else:
+                base_unit_kwargs[k] = v
 
         return func(*args, **base_unit_kwargs)
     return inner
