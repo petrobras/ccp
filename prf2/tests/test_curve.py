afa012c0b9c6bdf7d679affd0f86b6fbe9bdad4b
raphaeltimbo <raphaelts@gmail.com>
2018-04-18 17:40:49 -0300

    Test performance values.

--- prf2/tests/test_curve.py
+++ prf2/tests/test_curve.py
@@ -16,8 +16,8 @@ def test_raise_1_point():
 @pytest.fixture
 def curve0():
     suc = State.define(p=Q_(1, 'bar'), T=300, fluid='co2')
-    disch = State.define(p=Q_(2, 'bar'), T=310, fluid='co2')
-    disch1 = State.define(p=Q_(2.5, 'bar'), T=315, fluid='co2')
+    disch = State.define(p=Q_(2, 'bar'), T=370, fluid='co2')
+    disch1 = State.define(p=Q_(2.5, 'bar'), T=375, fluid='co2')
     p0 = Point(suc=suc, disch=disch, flow_v=1, speed=1)
     p1 = Point(suc=suc, disch=disch1, flow_v=2, speed=1)
     return Curve([p0, p1])
@@ -34,10 +34,14 @@ def test_curve_disch_parameters(curve0):
     assert curve0.disch.p().units == 'pascal'
     assert_allclose(curve0.disch.p(), np.array([200000., 250000.]))
     assert curve0.disch.T().units == 'kelvin'
-    assert_allclose(curve0.disch.T(), np.array([310., 315.]))
+    assert_allclose(curve0.disch.T(), np.array([370., 375.]))
 
 
 def test_curve_performance_parameters(curve0):
     assert curve0.head.units == 'joule/kilogram'
     assert curve0.eff.units == ureg.dimensionless
     assert curve0.power.units == 'kilowatt'
+    assert_allclose(curve0.head, np.array([43527.78012, 57942.686265]))
+    assert_allclose(curve0.eff, np.array([0.709246, 0.881994]), rtol=1e-6)
+    assert_allclose(curve0.power, np.array([108.813998, 232.958348]))
+
