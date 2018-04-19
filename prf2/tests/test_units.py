321e48d1be0e4187ce09d59a2da6163033794069
raphaeltimbo <raphaelts@gmail.com>
2018-04-19 16:10:10 -0300

    Add tests for b and D.

--- prf2/tests/test_units.py
+++ prf2/tests/test_units.py
@@ -10,17 +10,19 @@ def test_new_units_loaded():
 @pytest.fixture
 def auxiliary_function():
     @check_units
-    def func(p=None, T=None, speed=None, flow_v=None, flow_m=None, h=None, s=None):
-        return p, T, speed, flow_v, flow_m, h, s
+    def func(p=None, T=None, speed=None, flow_v=None, flow_m=None, h=None,
+             s=None, b=None, D=None):
+        return p, T, speed, flow_v, flow_m, h, s, b, D
     return func
 
 
 def test_units(auxiliary_function):
-    results = auxiliary_function(p=1, T=1, speed=1, flow_v=1, flow_m=1, h=1, s=1)
+    results = auxiliary_function(p=1, T=1, speed=1, flow_v=1, flow_m=1, h=1,
+                                 s=1, b=1, D=1)
     # check if all available units are tested
     assert len(results) == len(units)
 
-    p, T, speed, flow_v, flow_m, h, s = results
+    p, T, speed, flow_v, flow_m, h, s, b, D = results
 
     assert p.magnitude == 1
     assert p.units == 'pascal'
@@ -42,6 +44,12 @@ def test_units(auxiliary_function):
 
     assert s.magnitude == 1
     assert s.units == 'joule/(kelvin kilogram)'
+
+    assert b.magnitude == 1
+    assert b.units == 'meter'
+
+    assert D.magnitude == 1
+    assert D.units == 'meter'
 
 
 def test_unit_Q_(auxiliary_function):
@@ -50,11 +58,13 @@ def test_unit_Q_(auxiliary_function):
                                  flow_v=Q_(1, 'meter**3/second'),
                                  flow_m=Q_(1, 'kilogram/second'),
                                  h=Q_(1, 'joule/kilogram'),
-                                 s=Q_(1, 'joule/(kelvin kilogram)'))
+                                 s=Q_(1, 'joule/(kelvin kilogram)'),
+                                 b=Q_(1, 'meter'),
+                                 D=Q_(1, 'meter'))
     # check if all available units are tested
     assert len(results) == len(units)
 
-    p, T, speed, flow_v, flow_m, h, s = results
+    p, T, speed, flow_v, flow_m, h, s, b, D = results
 
     assert p.magnitude == 1
     assert p.units == 'pascal'
@@ -77,6 +87,12 @@ def test_unit_Q_(auxiliary_function):
     assert s.magnitude == 1
     assert s.units == 'joule/(kelvin kilogram)'
 
+    assert b.magnitude == 1
+    assert b.units == 'meter'
+
+    assert D.magnitude == 1
+    assert D.units == 'meter'
+
 
 def test_unit_Q_conversion(auxiliary_function):
     results = auxiliary_function(p=Q_(1, 'bar'), T=Q_(1, 'celsius'),
@@ -84,12 +100,14 @@ def test_unit_Q_conversion(auxiliary_function):
                                  flow_v=Q_(1, 'foot**3/second'),
                                  flow_m=Q_(1, 'lb/second'),
                                  h=Q_(1, 'btu/lb'),
-                                 s=Q_(1, 'btu/(degF lb)'))
+                                 s=Q_(1, 'btu/(degF lb)'),
+                                 b=Q_(1, 'inches'),
+                                 D=Q_(1, 'inches'))
 
     # check if all available units are tested
     assert len(results) == len(units)
 
-    p, T, speed, flow_v, flow_m, h, s = results
+    p, T, speed, flow_v, flow_m, h, s, b, D = results
 
     assert p.magnitude == 1e5
     assert p.units == 'pascal'
@@ -112,4 +130,9 @@ def test_unit_Q_conversion(auxiliary_function):
     assert s.magnitude == 4186.8
     assert s.units == 'joule/(kelvin kilogram)'
 
+    assert b.magnitude == 0.0254
+    assert b.units == 'meter'
+
+    assert D.magnitude == 0.0254
+    assert D.units == 'meter'
 
