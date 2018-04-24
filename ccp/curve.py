154bd99e3e1440c6eb536cc5362121c952e01a06
raphaeltimbo <raphaelts@gmail.com>
2018-04-24 17:09:01 -0300

    Move plot to external function.

--- ccp/curve.py
+++ ccp/curve.py
@@ -1,6 +1,48 @@
+import numpy as np
 import matplotlib.pyplot as plt
+import warnings
+from scipy.interpolate import UnivariateSpline
 from collections import UserList
 from ccp import Q_
+
+
+def plot_func(self, attr):
+    def inner(*args, **kwargs):
+        ax = kwargs.get('ax', None)
+
+        if ax is None:
+            ax = plt.gca()
+
+        values = []
+
+        for point in self:
+            point_attr = getattr(point, attr)
+            if callable(point_attr):
+                values.append(getattr(point_attr(), 'magnitude'))
+                units = getattr(getattr(point, attr)(), 'units')
+            else:
+                values.append(getattr(point_attr, 'magnitude'))
+                units = getattr(getattr(point, attr), 'units')
+
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            interpolated_curve = UnivariateSpline(self.flow_v.magnitude, values)
+
+        flow_v_range = np.linspace(min(self.flow_v.magnitude),
+                                   max(self.flow_v.magnitude),
+                                   30)
+        values_range = interpolated_curve(flow_v_range)
+
+        ax.plot(flow_v_range, values_range)
+        ax.set_xlabel(f'Volumetric flow ({self.flow_v.units:P~})')
+        ax.set_ylabel(f'{attr} ({units:P~})')
+
+        if kwargs.get('draw_points') is True:
+            ax.scatter(self.flow_v.magnitude, values)
+
+        return ax
+
+    return inner
 
 
 class _CurveState(UserList):
@@ -19,8 +61,8 @@ class _CurveState(UserList):
         for attr in ['p', 'T', 'h', 's']:
             func = self.state_parameter(attr)
             setattr(self, attr, func)
-            plot_func = self.plot(attr)
-            setattr(self, attr + '_plot', plot_func)
+            plot = plot_func(self, attr)
+            setattr(self, attr + '_plot', plot)
 
     def state_parameter(self, attr):
         def inner(*args, **kwargs):
@@ -32,28 +74,6 @@ class _CurveState(UserList):
             units = getattr(getattr(point, attr)(), 'units')
 
             return Q_(values, units)
-
-        return inner
-
-    def plot(self, attr):
-        def inner(*args, **kwargs):
-            ax = kwargs.get('ax', None)
-
-            if ax is None:
-                ax = plt.gca()
-
-            values = []
-
-            for point in self:
-                values.append(getattr(getattr(point, attr)(), 'magnitude'))
-
-            units = getattr(getattr(point, attr)(), 'units')
-
-            ax.plot(self.flow_v.magnitude, values)
-            ax.set_xlabel(f'Volumetric flow ({self.flow_v.units:P~})')
-            ax.set_ylabel(f'{attr} ({units:P~})')
-
-            return ax
 
         return inner
 
@@ -92,10 +112,12 @@ class Curve(UserList):
             values = []
             for point in self:
                 values.append(getattr(getattr(point, param), 'magnitude'))
-
             units = getattr(getattr(point, param), 'units')
 
             setattr(self, param, Q_(values, units))
+
+            plot = plot_func(self, param)
+            setattr(self, param + '_plot', plot)
 
 
 class NonDimensionalCurve(UserList):
