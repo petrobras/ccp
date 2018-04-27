68ab6bf730ec815110f48a1e6667e0f0077abc6a
raphaeltimbo <raphaelts@gmail.com>
2018-04-27 17:34:16 -0300

    Add speed text to curves.

--- ccp/curve.py
+++ ccp/curve.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import warnings
 from scipy.interpolate import UnivariateSpline
-from collections import UserList
 from ccp import Q_
 
 
@@ -35,6 +34,17 @@ def plot_func(self, attr):
         values_range = interpolated_curve(flow_v_range)
 
         ax.plot(flow_v_range, values_range, **kwargs)
+
+        delta_x_graph = ax.get_xlim()[1] - ax.get_xlim()[0]
+        delta_y_graph = ax.get_ylim()[1] - ax.get_ylim()[0]
+
+        curve_tan = ((values_range[-1] - values_range[-2]) / delta_y_graph) / \
+                    ((flow_v_range[-1] - flow_v_range[-2]) / delta_x_graph)
+        text_angle = np.arctan(curve_tan)
+        text_angle = Q_(text_angle, 'rad').to('deg').magnitude
+
+        ax.text(flow_v_range[-1], values_range[-1], f'{self.speed:.0f}',
+                ha='left', va='top', rotation=text_angle)
         ax.set_xlabel(f'Volumetric flow ({self.flow_v.units:P~})')
         ax.set_ylabel(f'{attr} ({units:P~})')
 
@@ -54,9 +64,10 @@ class _CurveState:
     (100000, 100000) pascal
 
     """
-    def __init__(self, points, flow_v):
+    def __init__(self, points, flow_v, speed):
         self.flow_v = flow_v
         self.points = points
+        self.speed = speed
 
         # set a method for each suction attribute in the list
         for attr in ['p', 'T', 'h', 's']:
@@ -109,8 +120,10 @@ class Curve:
             if self.speed != point.speed:
                 raise ValueError('Speed for each point should be equal')
 
-        self.suc = _CurveState([p.suc for p in self], flow_v=self.flow_v)
-        self.disch = _CurveState([p.disch for p in self], flow_v=self.flow_v)
+        self.suc = _CurveState([p.suc for p in self],
+                               flow_v=self.flow_v, speed=self.speed)
+        self.disch = _CurveState([p.disch for p in self],
+                                 flow_v=self.flow_v, speed=self.speed)
 
         for param in ['head', 'eff', 'power', 'phi', 'psi']:
             values = []
