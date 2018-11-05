04ad407ef05a7cff6b0b8cbc12d0d574aa958120
raphaeltimbo <raphaelts@gmail.com>
2018-11-04 23:46:39 -0200

    Change sphinx theme.

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
 # ccp documentation build configuration file, created by
 # sphinx-quickstart on Sun Nov  4 15:21:50 2018.
 #
@@ -83,7 +80,10 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+import sphinx_rtd_theme
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
