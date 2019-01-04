14e78eacd8626513e28465506a5a6f56bf0e2beb
Raphael Timbo Silva <raphaelts@petrobras.com.br>
2019-01-04 10:47:33 -0200

    Add FlowOrifice test.

---
+++ ccp/tests/test_fo.py
@@ -0,0 +1,21 @@
+import pytest
+import ccp
+from numpy.testing import assert_allclose
+
+Q_ = ccp.Q_
+
+
+@pytest.fixture
+def fo0():
+    n2 = ccp.State.define(p=Q_(9.85, 'bar'), T=Q_(145, 'degC'), fluid='n2')
+    qm = Q_(71281, 'kg/hr')
+    delta_p = Q_(9.85 - 4, 'bar')
+    D = Q_(0.3032, 'm')
+
+    return ccp.FlowOrifice(n2, delta_p, qm, D)
+
+
+def test_flow_orifice(fo0):
+    assert_allclose(fo0.d.magnitude, 0.10960820617559523)
+    fo0.D = 0.5
+    assert_allclose(fo0.d.magnitude, 0.11028294814259916)
