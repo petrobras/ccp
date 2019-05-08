159f98196ac5b7c0107cb96e489e5e3ad9d5fd05
raphaeltimbo <raphaelts@gmail.com>
2019-05-08 14:24:35 -0300

    Change read_csv to read csv with several curves

--- ccp/data_io/read_csv.py
+++ ccp/data_io/read_csv.py
@@ -1,4 +1,18 @@
-"""Module to read points from csv files created with engauge."""
+"""Module to read points from csv files created with engauge.
+
+The csv files should be generated with engauge with the following procedure:
+Inside engauge digitizer:
+ - Edit -> Paste as new
+ - On Axis Point -> add 3 reference points
+ - Settings -> Curves -> With 'New', add how many curves will be collected
+ - Segment fill
+ - Select the curve (e.g. Curve1)
+ - Select the points
+ - Select the next curve and points...
+ - Settings -> Export Setup -> Select:
+ - Raws X's and Y's ; All curves on each line
+
+"""
 import csv
 import numpy as np
 from scipy.interpolate import UnivariateSpline
@@ -6,112 +20,18 @@ from tqdm import tqdm
 from ccp import Q_, State, Point
 
 
-def _interpolated_curve_from_csv(file):
-    """Convert from csv file to interpolated curve.
+def load_curves(file_path):
+    """Generate curves dict from file_path."""
+    curves = {}
 
-    Function to convert from csv generated with engauge digitizer to an
-    interpolated curve.
-    """
-    flow_values = []
-    parameter = []
-
-    with open(file) as csv_file:
-        data = csv.reader(csv_file)
-        next(data)
-        for row in data:
-            flow_values.append(float(row[0]))
-            parameter.append(float(row[1]))
-
-    parameter_interpolated_curve = UnivariateSpline(flow_values, parameter)
-
-    return parameter_interpolated_curve, flow_values
-
-
-def get_points_from_csv(case_path, speed, parameters, number_of_points=6):
-    """Generate number of points from parameters interpolated curves."""
-    parameters_curves = {}
-    flow_all_values = []
-
-    for param in parameters:
-        param_file = case_path / f'{param}_{speed}.csv'
-        param_curve, flow_values = _interpolated_curve_from_csv(param_file)
-        flow_all_values += flow_values
-        parameters_curves[param] = param_curve
-
-    flow_v = np.linspace(min(flow_all_values), max(flow_all_values),
-                         number_of_points)
-
-    parameters_values = {'flow_v': flow_v}
-
-    for param in parameters:
-        parameters_values[param] = parameters_curves[param](flow_v)
-
-    return parameters_values
-
-
-def get_case_speeds(case_path, parameter):
-    """Get speed values for the given case path."""
-    param_files = case_path.glob(f'*{parameter}*.csv')
-    speed_values = [int(f.stem.split('_')[-1]) for f in param_files]
-
-    return speed_values
-
-
-def create_prf_points(case_path, parameters, suc, speed):
-    """Create points for each speed."""
-    parameters_names = [k for k, v in parameters.items()
-                        if k not in ['speed', 'flow_v']]
-
-    points_values = get_points_from_csv(case_path, speed, parameters_names)
-
-    points = []
-    for i in tqdm(range(len(points_values[parameters_names[0]]))):
-        kwargs = {'suc': suc}
-        disch_kwargs = {}
-        for name, unit in parameters.items():
-            if name == 'speed':
-                kwargs[name] = Q_(speed, unit)
-            elif name == 'eff':
-                parameter_magnitude = points_values[name][i]
-                if parameter_magnitude > 1:
-                    parameter_magnitude /= 100
-                kwargs[name] = Q_(parameter_magnitude, unit)
-            elif name == 'disch-temperature':
-                parameter_magnitude = points_values[name][i]
-                disch_kwargs['T'] = Q_(parameter_magnitude, unit)
-            elif name == 'disch-pressure':
-                parameter_magnitude = points_values[name][i]
-                disch_kwargs['p'] = Q_(parameter_magnitude, unit)
-            elif name == 'pressure-ratio':
-                disch_kwargs['p'] = points_values[name][i] * suc.p()
+    with open(str(file_path)) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            if row[0] == 'x':
+                current_curve = row[1]
+                curves[current_curve] = {'x': [], 'y': []}
             else:
-                parameter_magnitude = points_values[name][i]
-                kwargs[name] = Q_(parameter_magnitude, unit)
-            if len(disch_kwargs) == 2:
-                kwargs['disch'] = State.define(fluid=suc.fluid, **disch_kwargs)
+                curves[current_curve]['x'].append(float(row[0]))
+                curves[current_curve]['y'].append(float(row[1]))
 
-        points.append(Point(**kwargs))
-
-    return points
-
-
-def load_case(case_path, parameters, suc):
-    """Load from case path, given parameters and suction conditions."""
-
-    param = None
-    for p in ['head', 'eff', 'disch-temperature']:
-        if p in parameters:
-            param = p
-        if param is not None:
-            break
-
-    speed_values = get_case_speeds(case_path, param)
-
-    points = []
-    for speed in tqdm(speed_values,
-                      desc='Getting points for each speed', unit='speed'):
-        points += create_prf_points(case_path, parameters, suc, speed)
-
-    return points
-
-
+    return curves
