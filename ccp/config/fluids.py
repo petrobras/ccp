ea3df5c3c2f045ad50a09dc888c08367f77cee49
raphaeltimbo <raphaelts@gmail.com>
2020-06-23 19:25:10 -0300

    Remove float diff from normalize

--- ccp/config/fluids.py
+++ ccp/config/fluids.py
@@ -80,5 +80,7 @@ def normalize_mix(molar_fractions):
     for i, comp in enumerate(molar_fractions):
         molar_fractions[i] = comp / total
 
-    diff = sum(molar_fractions) - 1.
-    molar_fractions[0] = molar_fractions[0] - diff
+    while sum(molar_fractions) != 1.:
+        diff = sum(molar_fractions) - 1.
+        molar_fractions[i] = molar_fractions[i] - diff
+        i += 1
