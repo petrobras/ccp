b64a57574cbc14a7a7a6772bbbb678ab8c7e3c0d
raphaeltimbo <raphaelts@gmail.com>
2021-04-19 13:55:00 -0300

    Change title to remove style
    The title is appended to the title in other places and this would result
    in titles such as
    <title>Topic Guides — p style='font-family: Fira Code Medium, monospace 'ccp'</title>

--- docs/conf.py
+++ docs/conf.py
@@ -100,7 +100,7 @@ import sphinx_rtd_theme
 html_theme = "sphinx_book_theme"
 html_logo = "_static/ccp.png"
 html_favicon = "_static/favicon.ico"
-html_title = "<p style='font-family: Fira Code Medium, monospace'>ccp</p>"
+html_title = "ccp"
 html_sidebars = {}
 html_theme_options = {
     "github_url": "https://github.com/raphaeltimbo/ccp",
