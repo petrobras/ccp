fa64a0a2262fa0ee8cd7087b5ed62adb7ee5864b
raphaeltimbo <raphaelts@gmail.com>
2021-05-04 10:21:19 -0300

    Define custom getstate and setstate for point
    We had to set a custom getstate and setstate for point since the plot
    functions which are defined when the point is

--- ccp/cprofile/cprofile.py
+++ ccp/cprofile/cprofile.py
@@ -1,9 +1,19 @@
+"""Make a cProfile from main ccp functions.
+
+To use it:
+$ python cprofile.py <function name>
+
+This will create a .prof file that can be visualized with snakeviz
+$ pip install snakeviz
+$ snakeviz <prof file name>.prof
+
+"""
 import cProfile
 import subprocess
 import sys
 from pathlib import Path
 
-from ccp import Q_, State, Point, Impeller, read_csv
+from ccp import Q_, State, Point, Impeller
 
 
 def generate_label(caller):
@@ -50,6 +60,8 @@ def impeller():
         speed=Q_(11145, "RPM"),
         head=Q_(179.275, "kJ/kg"),
         eff=0.826357,
+        b=Q_(28.5, "mm"),
+        D=Q_(365, "mm"),
     )
     p1 = Point(
         suc=suc,
@@ -57,36 +69,47 @@ def impeller():
         speed=Q_(11145, "RPM"),
         head=Q_(173.057, "kJ/kg"),
         eff=0.834625,
+        b=Q_(28.5, "mm"),
+        D=Q_(365, "mm"),
     )
 
-    imp1 = Impeller([p0, p1], b=Q_(28.5, "mm"), D=Q_(365, "mm"))
+    imp1 = Impeller(
+        [p0, p1],
+    )
 
 
-def create_prf_points():
-    case_a = Path().home() / "ccp/ccp/tests/data/"
-    fluid = {
-        "methane": 0.7878,
-        "ethane": 0.0917,
-        "propane": 0.0562,
-        "butane": 0.0146,
-        "isobutane": 0.0086,
-        "pentane": 0.0028,
-        "isopentane": 0.0023,
-        "hexane": 0.0008,
-        "heptane": 0,
-        "octane": 0,
-        "nonane": 0,
-        "decane": 0,
-        "undecane": 0,
-        "dodecane": 0,
-        "nitrogen": 0.0049,
-        "hydrogen sulfide": 0.00003,
-        "carbon dioxide": 0.0303,
-        "water": 0.00001,
-    }
-    suc = State.define(p=Q_(250.5, "bar"), T=Q_(40, "degC"), fluid=fluid)
-    parameters = dict(flow_v="m**3/min", speed="RPM", head="m*kgf/kg", eff=None)
-    read_csv.create_prf_points(case_a, parameters, suc, 7229)
+def create_ccp_points():
+    composition_fd = dict(
+        n2=0.4,
+        co2=0.22,
+        methane=92.11,
+        ethane=4.94,
+        propane=1.71,
+        ibutane=0.24,
+        butane=0.3,
+        ipentane=0.04,
+        pentane=0.03,
+        hexane=0.01,
+    )
+    suc_fd = State.define(p=Q_(3876, "kPa"), T=Q_(11, "degC"), fluid=composition_fd)
+
+    test_dir = Path.home() / "ccp/ccp/tests"
+    curve_path = test_dir / "data"
+    curve_name = "normal"
+
+    imp_fd = Impeller.load_from_engauge_csv(
+        suc=suc_fd,
+        curve_name=curve_name,
+        curve_path=curve_path,
+        b=Q_(10.6, "mm"),
+        D=Q_(390, "mm"),
+        number_of_points=6,
+        flow_units="kg/hr",
+        head_units="kJ/kg",
+    )
+    new_fluid = {"co2": 0.7, "n2": 0.3}
+    new_suc = State.define(p=Q_(10, "bar"), T=Q_(40, "degC"), fluid=new_fluid)
+    imp_conv = Impeller.convert_from(imp_fd, suc=new_suc, find="speed")
 
 
 if __name__ == "__main__":
