fa9dd4c34109e014803f3b3a3cc2f2c7b926cf41
raphaeltimbo <raphaelts@gmail.com>
2021-06-03 15:44:41 -0300

    rollback

--- docs/conf.py
+++ docs/conf.py
@@ -15,9 +15,9 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-import sys
 
-sys.path.insert(0, os.path.abspath("."))
+# import sys
+# sys.path.insert(0, os.path.abspath("."))
 import ccp
 
 # General information about the project.
