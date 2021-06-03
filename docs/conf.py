9467a902056381c8703f9ea42d559fffad665c82
raphaeltimbo <raphaelts@gmail.com>
2021-06-03 16:07:32 -0300

    Readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -16,8 +16,9 @@
 #
 import os
 
-# import sys
-# sys.path.insert(0, os.path.abspath("."))
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.pardir))
 import ccp
 
 # General information about the project.
