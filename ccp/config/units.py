b1e0650f748d6a4ae1f6e9c695644e9620ad02ff
raphaeltimbo <raphaelts@petrobras.com.br>
2022-03-31 23:22:50 -0300

    Add check units from ROSS
    We are now using the improved version of check units from ROSS. In this
    case we need to do some modifications due to issues with variables such
    as G_s (the s will be checked as entropy after splitting on '_'). On
    solution to this would be to look up the complete name first in the
    unit dictionary, if it is there we don't go to the split on '_' step.

--- ccp/config/units.py
+++ ccp/config/units.py
@@ -1,16 +1,79 @@
+"""This module deals with units conversion in the ccp library."""
 import inspect
+import warnings
 from functools import wraps
+from pathlib import Path
 
-from .. import Q_
+import pint
+
+new_units_path = Path(__file__).parent / "new_units.txt"
+ureg = pint.get_application_registry()
+if isinstance(ureg.get(), pint.registry.LazyRegistry):
+    ureg = pint.UnitRegistry()
+    ureg.load_definitions(str(new_units_path))
+    # set ureg to make pickle possible
+    pint.set_application_registry(ureg)
+
+Q_ = ureg.Quantity
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    pint.Quantity([])
+
+__all__ = ["Q_", "check_units"]
 
 units = {
-    "p": "pascal",
-    "disch_p": "pascal",
-    "T": "kelvin",
-    "rho": "kilogram/m**3",
+    "E": "N/m**2",
+    "Gs": "N/m**2",
+    "rho": "kg/m**3",
+    "density": "kg/m**3",
+    "L": "meter",
+    "idl": "meter",
+    "idr": "meter",
+    "odl": "meter",
+    "odr": "meter",
+    "id": "meter",
+    "od": "meter",
+    "i_d": "meter",
+    "o_d": "meter",
     "speed": "radian/second",
+    "frequency": "radian/second",
+    "mx": "kg",
+    "my": "kg",
+    "Ip": "kg*m**2",
+    "Id": "kg*m**2",
+    "width": "meter",
+    "depth": "meter",
+    "thickness": "meter",
+    "pitch": "meter",
+    "height": "meter",
+    "radius": "meter",
+    "diameter": "meter",
+    "clearance": "meter",
+    "length": "meter",
+    "area": "meter**2",
+    "unbalance_magnitude": "kg*m",
+    "unbalance_phase": "rad",
+    "pressure": "pascal",
+    "p": "pascal",
+    "temperature": "degK",
+    "T": "degK",
+    "velocity": "m/s",
+    "angle": "rad",
+    "arc": "rad",
+    "convection": "W/(m²*degK)",
+    "conductivity": "W/(m*degK)",
+    "expansion": "1/degK",
+    "stiffness": "N/m",
+    "weight": "N",
+    "load": "N",
+    "force": "N",
+    "torque": "N*m",
+    "flowv": "m³/s",
     "flow_v": "meter**3/second",
     "flow_m": "kilogram/second",
+    "fit": "m",
+    "viscosity": "pascal*s",
     "h": "joule/kilogram",
     "s": "joule/(kelvin kilogram)",
     "b": "meter",
@@ -19,10 +82,38 @@ units = {
     "eff": "dimensionless",
     "power": "watt",
 }
+for i, unit in zip(["k", "c"], ["N/m", "N*s/m"]):
+    for j in ["x", "y", "z"]:
+        for k in ["x", "y", "z"]:
+            units["".join([i, j, k])] = unit
 
 
 def check_units(func):
-    """Wrapper to check and convert units to base_units."""
+    """Wrapper to check and convert units to base_units.
+    If we use the check_units decorator in a function the arguments are checked,
+    and if they are in the dictionary, they are converted to the 'default' unit given
+    in the dictionary.
+    The check is carried out by splitting the argument name on '_', and checking
+    if any of the names are in the dictionary. So an argument such as 'inlet_pressure',
+    will be split into ['inlet', 'pressure'], and since we have the name 'pressure'
+    in the dictionary mapped to 'Pa', we will automatically convert the value to
+    this default unit.
+    For example:
+    >>> units = {
+    ... "L": "meter",
+    ... }
+    >>> @check_units
+    ... def foo(L=None):
+    ...     print(L)
+    ...
+    If we call the function with the argument as a float:
+    >>> foo(L=0.5)
+    0.5
+    If we call the function with a pint.Quantity object the value is automatically
+    converted to the default:
+    >>> foo(L=Q_(0.5, 'inches'))
+    0.0127
+    """
 
     @wraps(func)
     def inner(*args, **kwargs):
@@ -30,35 +121,45 @@ def check_units(func):
         args_names = inspect.getfullargspec(func)[0]
 
         for arg_name, arg_value in zip(args_names, args):
-            if arg_name in units:
-                try:
-                    base_unit_args.append(arg_value.to(units[arg_name]))
-                except AttributeError:
-                    base_unit_args.append(Q_(arg_value, units[arg_name]))
+            names = arg_name.split("_")
+            if arg_name not in names:
+                names.append(arg_name)
+            for name in names:
+                if name in units and arg_value is not None:
+                    # For now, we only return the magnitude for the converted Quantity
+                    # If pint is fully adopted by ross in the future, and we have all Quantities
+                    # using it, we could remove this, which would allows us to use pint in its full capability
+                    try:
+                        base_unit_args.append(arg_value.to(units[name]))
+                    except AttributeError:
+                        try:
+                            base_unit_args.append(Q_(arg_value, units[name]))
+                        except TypeError:
+                            # Handle erros that we get with bool for example
+                            base_unit_args.append(arg_value)
+                    break
             else:
                 base_unit_args.append(arg_value)
 
         base_unit_kwargs = {}
         for k, v in kwargs.items():
-            if k in units and v is not None:
-                try:
-                    base_unit_kwargs[k] = v.to(units[k])
-                except AttributeError:
-                    base_unit_kwargs[k] = Q_(v, units[k])
+            names = k.split("_")
+            if k not in names:
+                names.append(k)
+            for name in names:
+                if name in units and v is not None:
+                    try:
+                        base_unit_kwargs[k] = v.to(units[name])
+                    except AttributeError:
+                        try:
+                            base_unit_kwargs[k] = Q_(v, units[name])
+                        except TypeError:
+                            # Handle erros that we get with bool for example
+                            base_unit_kwargs[k] = v
+                    break
             else:
                 base_unit_kwargs[k] = v
 
         return func(*base_unit_args, **base_unit_kwargs)
 
     return inner
-
-
-def change_data_units(x_data, y_data, x_units=None, y_units=None):
-    """Change data units for plotting."""
-    if x_units is not None:
-        x_data = x_data.to(x_units)
-
-    if y_units is not None:
-        y_data = y_data.to(y_units)
-
-    return x_data, y_data
