d16c91b79cedf8b1e3a5421c3290140de1979213
raphaeltimbo <raphaelts@gmail.com>
2022-05-07 14:07:32 -0300

    Add tests folder to the pypi distribution

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ setup(
     long_description_content_type="text/markdown",
     author="Raphael Timbó",
     author_email="raphaelts@gmail.com",
-    packages=find_packages(exclude=("tests",)),
     package_data={"ccp.config": ["new_units.txt"]},
     python_requires=">=3.6",
     install_requires=[
