7de637845b349f4f2ab271821c050604226949e7
raphaeltimbo <raphaelts@petrobras.com.br>
2022-11-25 08:07:08 -0300

    Change update to restore files
    If a file is modified the merge will fail, so we have to restore all
    files before we try to make the git pull.

--- update.bat
+++ update.bat
@@ -1,7 +1,11 @@
 @echo off
 Script to update ccp from git
-echo Updating ccp...
+echo O update ira sobescrever qualquer modificacao feita aos arquivos na pasta.
+set /p continuar="Deseja continuar? [y/n]"
+if /i "%continuar%" neq "y" goto end
 git init
+git restore .
 git remote add codigo https://codigo.petrobras.com.br/equipamentos-dinamicos/ccp.git
 git pull codigo master
 pause
+:end
