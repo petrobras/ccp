4d7678e622d3e8c48626ee556cda5e3ef6a9bf27
raphaeltimbo <raphaelts@petrobras.com.br>
2023-04-06 18:54:48 -0300

    Update doc requirements

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,6 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
 
 html_theme = "sphinx_book_theme"
 html_logo = "_static/ccp.png"
