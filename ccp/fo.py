6cb39169c1a4ba105c0861a04baa8340a3405e92
Raphael Nascimento Cisneiros <raphael.cisneiros@petrobras.com.br>
2023-04-14 21:21:10 +0000

    update FlowOrifice class - rev.1

--- ccp/fo.py
+++ ccp/fo.py
@@ -1,108 +1,105 @@
 """Flow orifice."""
 import numpy as np
-from pint.errors import DimensionalityError
 from scipy.optimize import newton
-
-from . import Q_
+from ccp.config.units import check_units
+from ccp import Q_
 
 
 class FlowOrifice:
-    def __init__(self, state=None, delta_p=None, qm=None, D=None, tappings="flange"):
+    @check_units
+    def __init__(
+        self,
+        state=None,
+        delta_p=None,
+        D=None,
+        d=None,
+        tappings="flange",
+        qm = None,
+    ):
         self.state = state
         self.delta_p = delta_p
-        self.qm = qm
-        self.D = D.to("m")
+        self.D = D
+        self.d = d.to('m')
         self.tappings = tappings
 
-        d = newton(
-            calc_d, 0.1, args=(self.state, self.delta_p, self.qm, self.D, self.tappings)
+        if tappings == "corner" or tappings == "D D/2" or tappings == "flange":
+            pass
+        else:
+            raise ValueError('tappings must be "corner", "D D/2" or "flange"')
+
+        if qm is None:
+            self.qm = getattr(self,"calc_flow")()
+        else:
+            self.qm = qm
+
+    def calc_flow(self):
+
+        D = self.D
+        d = self.d
+        delta_p = self.delta_p
+        tappings = self.tappings
+        state = self.state
+        p1 = state.p()
+
+        p2 = p1 - delta_p
+
+        beta = d / D
+        mu = state.viscosity()
+        rho = state.rho()
+        k = state.kv()
+
+        e = 1 - (0.351 + 0.256 * (beta**4) + 0.93 * (beta**8)) * (
+            1 - (p2 / p1) ** (1 / k)
         )
-        self.d = Q_(float(d), "m")
-        _, self.beta, self.C, self.e, self.reynolds, self.A = calc(
-            self.d.magnitude, self.state, self.delta_p, self.qm, self.D, self.tappings
-        )
 
-    def update(self):
-        d = newton(
-            calc_d, 0.1, args=(self.state, self.delta_p, self.qm, self.D, self.tappings)
-        )
-        self.d = Q_(float(d), "m")
+        if tappings == "corner":
+            L1 = L2 = 0
+        elif tappings == "D D/2":
+            L1 = 1
+            L2 = 0.47
+        elif tappings == "flange":
+            L1 = L2 = Q_(0.0254, "m") / D
 
-    def __setattr__(self, name, value):
-        try:
-            if isinstance(getattr(self, name), Q_) and not isinstance(value, Q_):
-                super().__setattr__(name, Q_(value, getattr(self, name).units))
-            else:
-                super().__setattr__(name, value)
-
-        except AttributeError:
-            super().__setattr__(name, value)
+        M2 = 2 * L2 / (1 - beta)
 
 
-def calc(x, state=None, delta_p=None, qm=None, D=None, tappings="flange"):
-    try:
-        d = Q_(x, "m")
-    except DimensionalityError:
-        d = Q_(x.m, "m")
+        def update_Reyn(Reyn):
 
-    # calc beta
-    beta = d / D
+            global qm
 
-    p1 = state.p()
-    p2 = p1 + delta_p
-    rho = state.rho()
-    A = np.pi * D**2 / 4
-    u = qm / (rho * A)
-    reyn = (rho * u * D / state.viscosity()).to("dimensionless")
+            Reyn = Q_(Reyn, "dimensionless")
+            # calc C
+            C = (
+                0.5961
+                + 0.0261 * beta**2
+                - 0.216 * beta**8
+                + 0.000521 * (1e6 * beta / Reyn) ** 0.7
+                + (0.0188 + 0.0063 * (19000 * beta / Reyn) ** 0.8)
+                * beta**3.5
+                * (1e6 / Reyn) ** 0.3
+                + (0.043 + 0.080 * np.e ** (-10 * L1) - 0.123 * np.e ** (-7 * L1))
+                * (1 - 0.11 * (19000 * beta / Reyn) ** 0.8)
+                * (beta**4 / (1 - beta**4))
+                - 0.031 * (M2 - 0.8 * M2**1.1) * beta**1.3
+            )
 
-    if tappings == "corner":
-        L1 = L2 = 0
-    elif tappings == "D D/2":
-        L1 = 1
-        L2 = 0.47
-    elif tappings == "flange":
-        L1 = L2 = Q_(0.0254, "m") / D
-    else:
-        raise ValueError('tappings must be "corner", "D D/2" or "flange"')
+            if D < Q_(71.12, "mm"):
+                C += 0.011 * (0.75 - beta) * (2.8 - D / Q_(25.4, "mm"))
 
-    M2 = 2 * L2 / (1 - beta)
+            qm = (
+                C
+                / (np.sqrt(1 - beta**4))
+                * e
+                * (np.pi / 4)
+                * d**2
+                * np.sqrt(2 * delta_p * rho)
+            )
 
-    # calc C
-    C = (
-        0.5961
-        + 0.0261 * beta**2
-        - 0.216 * beta**8
-        + 0.000521 * (1e6 * beta / reyn) ** 0.7
-        + (0.0188 + 0.0063 * (19000 * beta / reyn) ** 0.8)
-        * beta**3.5
-        * (1e6 / reyn) ** 0.3
-        + (0.043 + 0.080 * np.e ** (-10 * L1) - 0.123 * np.e ** (-7 * L1))
-        * (1 - 0.11 * (19000 * beta / reyn) ** 0.8)
-        * (beta**4 / (1 - beta**4))
-        - 0.031 * (M2 - 0.8 * M2**1.1) * beta**1.3
-    )
+            Reyn_qm = (4 * qm / (mu * np.pi * D)).to("dimensionless").magnitude
 
-    if D < Q_(71.12, "mm"):
-        C += 0.011 * (0.75 - beta) * (2.8 - D / 0.0254)
+            return abs(Reyn_qm - Reyn.magnitude)
 
-    # calc e
-    e = 1 - (0.351 + 0.256 * beta**4 + 0.93 * beta**8) * (
-        1 - (p2 / p1) ** (1 / state.kT())
-    )
+        newton(update_Reyn, 1e8, tol=1e-5)
 
-    # calc qm
-    qm_calc = (
-        C
-        / (np.sqrt(1 - beta**4))
-        * e
-        * (np.pi / 4)
-        * d**2
-        * np.sqrt(2 * delta_p * rho)
-    )
+        return qm.to("kg/s")
 
-    return qm - qm_calc, beta, e, C, reyn, A
-
-
-def calc_d(x, state=None, delta_p=None, qm=None, D=None, tappings="flange"):
-    r = calc(x, state, delta_p, qm, D, tappings)
-    return r[0]
