314708fba2613e650a978905be0506c8eedad6f8
raphaeltimbo <raphaelts@petrobras.com.br>
2024-01-05 18:35:53 -0300

    Fix surge line errors

--- ccp/impeller.py
+++ ccp/impeller.py
@@ -117,14 +117,22 @@ class ImpellerPlotFunction:
         # store values for surge flow and attr value in lists so that we can plot them
         surge_flow_list = []
         surge_attr_list = []
-        attr_units = kwargs.get(
-            f"{attr}_units", getattr(impeller_object.curves[0], attr).units
-        )
+        try:
+            attr_units = kwargs.get(
+                f"{attr}_units", r_getattr(impeller_object.curves[0], attr).units
+            )
+        except AttributeError:
+            attr_units = kwargs.get(
+                f"{attr}_units", r_getattr(impeller_object.curves[0], attr)().units
+            )
 
         for curve in impeller_object.curves:
             fig = r_getattr(curve, attr + "_plot")(fig=fig, plot_kws=plot_kws, **kwargs)
             surge_flow_list.append(curve.flow_v[0].to(flow_v_units).m)
-            surge_attr_list.append(r_getattr(curve, attr)[0].to(attr_units).m)
+            try:
+                surge_attr_list.append(r_getattr(curve, attr).to(attr_units).m)
+            except AttributeError:
+                surge_attr_list.append(r_getattr(curve, attr)().to(attr_units).m)
 
         if speed:
             current_curve = impeller_object.curve(speed=speed)
@@ -132,7 +140,13 @@ class ImpellerPlotFunction:
                 fig=fig, plot_kws=plot_kws, **kwargs
             )
             surge_flow_list.append(current_curve.flow_v[0].to(flow_v_units).m)
-            surge_attr_list.append(r_getattr(current_curve, attr)[0].to(attr_units).m)
+            try:
+                surge_attr_list.append(r_getattr(current_curve, attr).to(attr_units).m)
+            except AttributeError:
+                surge_attr_list.append(
+                    r_getattr(current_curve, attr)().to(attr_units).m
+                )
+
             if flow_v:
                 current_point = impeller_object.point(flow_v=flow_v, speed=speed)
                 fig = r_getattr(current_point, attr + "_plot")(
