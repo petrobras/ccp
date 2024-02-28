74a79afcf4f922153e00833e0a5d7b916319694d
raphaeltimbo <raphaelts@petrobras.com.br>
2024-02-28 16:27:37 -0300

    Remove redundant files
    These files are now inside the pages folder,therefore we need to remove
    these.

--- ccp/app/ccp_app.bat
+++ ccp/app/ccp_app.bat
@@ -2,5 +2,5 @@
 call %HOMEPATH%\anaconda3\Scripts\activate.bat
 set CCP_ENV=%CCP_ENV% || base
 call conda activate %CCP_ENV%
-streamlit run "ccp_app_back_to_back.py"
+streamlit run "ccp_app.py"
 pause
